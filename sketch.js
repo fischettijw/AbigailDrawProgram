778b4991602827e849f337267ea073e08ee3b83d
Joseph Fischetti <fischettijw@gmail.com>
2020-08-19 08:18:51 -0400

    stagged changes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 //  https://p5js.org/reference/#/p5.Vector/reflect      DRAW ARROW
 
-let btnClear, cbxRandomColors;
-let colorPicker, thicknessSlider;
+let ctrlFlag, btnClear, cbxRandomColors, colorPicker, thicknessSlider;
 let drawColor, penThickness;
 let smouseX, smouseY;
 let CAS, caS, cAs, Cas, CAs;
@@ -11,21 +10,28 @@ function setup(){
   initializeDraw();
 }
 
-function initializeDraw(){     // need to remove creates - after clear multiple imstances
+function initializeDraw(){
   strokeCap(ROUND);
   drawColor = color(255);
-  penThickness = 5;
+  penThickness = 10;
   background(0);
-  colorPicker = createColorPicker('#FFFFFF');
-  drawColor = colorPicker.color();
-  colorPicker.position(5, height + 5);
-  thicknessSlider = createSlider(1,120,10,1);
-  thicknessSlider.position(60,height+9);
-  cbxRandomColors = createCheckbox('Random Colors', false);
-  cbxRandomColors.position(200,height + 9);
-  btnClear = createButton('CLEAR');
-  btnClear.position(330,height+9);
-  btnClear.mousePressed(btnClearClicked);
+  if(ctrlFlag == null){;
+    colorPicker = createColorPicker(drawColor);
+    drawColor = colorPicker.color();
+    colorPicker.position(5, height + 5);
+    thicknessSlider = createSlider(1,120,penThickness,1);
+    thicknessSlider.position(60,height+9);
+    cbxRandomColors = createCheckbox('Random Colors', false);
+    cbxRandomColors.position(200,height + 9);
+    btnClear = createButton('CLEAR');
+    btnClear.position(330,height+9);
+    btnClear.mousePressed(btnClearClicked);
+    ctrlFlag = true;
+  }
+}
+
+function btnClearClicked(){
+   initializeDraw();
 }
 
 function draw(){
@@ -40,11 +46,10 @@ function draw(){
   if(mouseIsPressed){ drawLine(); }
 }
 
-function btnClearClicked(){
-   {initializeDraw();} 
-}
 
 function drawLine(){
+  if(mouseX >= width || mouseY >= height) { return;}
+
   CAS = keyIsDown(CONTROL)==true && keyIsDown(ALT)==true && keyIsDown(SHIFT)==true;
   caS = keyIsDown(CONTROL)==false && keyIsDown(ALT)==false && keyIsDown(SHIFT)==true;
   cAs = keyIsDown(CONTROL)==false && keyIsDown(ALT)==true && keyIsDown(SHIFT)==false;
@@ -55,16 +60,16 @@ function drawLine(){
   strokeWeight(penThickness);
 
   if (cAs){
-    line(smouseX,smouseY,mouseX,mouseY); }else 
-  if (CAs){
+    line(smouseX,smouseY,mouseX,mouseY); 
+  }else if (CAs){
     if (dist(smouseX,smouseY,mouseX, mouseY) > 5){    
        drawLineWithArrow(createVector(smouseX,smouseY), 
        createVector(mouseX,mouseY), drawColor);
-    } }else 
-  if (caS) {
+    } 
+  }else if (caS) {
     line(smouseX,smouseY,mouseX,smouseY);    
-    mouseY = smouseY; }else 
-  if (Cas) {
+    mouseY = smouseY; 
+  }else if (Cas) {
     line(smouseX,smouseY,smouseX,mouseY);    
     mouseX = smouseX; 
   }else{
@@ -89,4 +94,3 @@ function drawLineWithArrow(base, vec, myColor) {
   triangle(0, (arrowSize / 2) , 0, (-arrowSize / 2) , arrowSize , 0);
   pop();
 }
-
